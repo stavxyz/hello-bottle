9b9f6f09903d631d82da6fee9a587271c3b41f05
Sam Stavinoha <smlstvnh@gmail.com>
2015-12-04 11:45:09 -0500

    organize cli and funcs

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 # __about__
 __title__ = 'hello-bottle'
@@ -11,11 +12,20 @@ __license__ = 'Apache License, Version 2.0'
 # __about__
 
 
-
 from bottle import route, run
 
 @route('/')
 def hello():
     return "Hello World!\n"
 
-run(host='0.0.0.0', port=8080, debug=True)
+
+def main():
+    run(host='0.0.0.0', port=8080, debug=True)
+
+
+def cli():
+    main()
+
+
+if __name__ == '__main__':
+    cli()
