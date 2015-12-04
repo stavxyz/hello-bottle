c8a294b2c84543b19cec7db2b4b3bb775ce41f69
Sam Stavinoha <smlstvnh@gmail.com>
2015-12-04 11:37:22 -0500

    add pkg info

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
+
+# __about__
+__title__ = 'hello-bottle'
+__summary__ = 'It says hello.'
+__url__ = 'https://github.com/samstav/hello-bottle'
+__version__ = '1.0.0'
+__author__ = 'Rackers'
+__email__ = 'smlstvnh@gmail.com'
+__keywords__ = ['python', 'bottle', 'docker', 'rancher']
+__license__ = 'Apache License, Version 2.0'
+# __about__
+
+
+
 from bottle import route, run
 
 @route('/')
